17db2ee9dcdfc80773e2585f548b51b55b4f0752
Justus B <justus.c.brown@gmail.com>
2022-07-27 13:53:21 -0700

    put and delete methods for server work

--- db/query.sql
+++ db/query.sql
@@ -2,3 +2,10 @@ USE movies_db;
 
 SELECT * FROM movies;
 SELECT * FROM movie_reviews;
+
+UPDATE movie_reviews
+SET review_text = ?
+WHERE id = ?;
+
+DELETE FROM movies
+WHERE id = ?;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
 
@@ -9,9 +10,91 @@ app.use(express.urlencoded({extended:true}))
 
 // app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    console.log(req)
-    console.log(res)
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'password',
+        database: 'movies_db'
+      }, 
+      console.log(`database movies_db connected`)
+)
+
+//movies
+app.get('/api/movies', function(req, res){
+    console.log(req.body)
+    db.query('SELECT * FROM movies;', (err, data)=>{
+        if (err){
+            console.log(err)
+        }
+        res.json(data)
+    })
+
+})
+app.get('/api/movies/reviews', function(req, res){
+    console.log(req.body)
+    db.query('SELECT * FROM movie_reviews;', (err, data)=>{
+        if (err){
+            console.log(err)
+        }
+        res.json(data)
+    })
+
+})
+app.post('/api/add-movie', function(req, res){ //add a movie
+    console.log(req.body)
+    const value1 = req.body.title
+
+    console.log(value1)
+    db.query('INSERT INTO movies(movie_name) VALUES (?);', value1,  (err, data)=>{
+        if (err){
+            console.log(err)
+        }
+        res.json(data)
+    })
+})
+app.put('/api/update-review/:id', function(req, res){ //add a review
+    console.log(req.body)
+    console.log(req.params.id)
+
+    const value1 = req.body.text
+    const value2 = req.params.id
+
+    console.log(value1)
+    db.query('UPDATE movie_reviews SET review_text = ? WHERE id = ?;', [value1,value2],  (err, data)=>{
+        if (err){
+            console.log(err)
+        }
+        res.json(data)
+    })
+
+
+    // db.query('INSERT INTO movie_reviews(review_text, movie_id) VALUES ("I hated it ", 1),', value,  (err, data)=>{
+    //     if (err){
+    //         console.log(err)
+    //     }
+    //     res.json(data)
+    // })
+})
+app.delete('/api/movies/:id', function(req, res){ //add a review
+    console.log(req.params.id)
+    const value2 = req.params.id
+    db.query('DELETE FROM movies WHERE id = ?;', [value2],  (err, data)=>{
+        if (err){
+            console.log(err)
+        }
+        res.json(data)
+    })
+
+
+    // db.query('INSERT INTO movie_reviews(review_text, movie_id) VALUES ("I hated it ", 1),', value,  (err, data)=>{
+    //     if (err){
+    //         console.log(err)
+    //     }
+    //     res.json(data)
+    // })
 })
 
 
